6fa63bb8748420fdb6e5baa0a7de9fc8bf7fb526
Austin McCallister <austinmccallister0@gmail.com>
2024-03-31 18:34:47 -0400

    Got most of imaptosms working
    Still need to make a new google acc because it was flagged my tmobile but it seems to be working

---
+++ message/imaptosms/imaptosms.py
@@ -0,0 +1,41 @@
+import ssl
+import smtplib
+import json
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+CONFIG = None
+with open("./config.json", "r") as f:
+    CONFIG = json.load(f)
+
+def send_message(content: str, email: str) -> None:
+    """ Sends a message with IMAP - Used to send messages over SMS
+    
+    Parameters:
+    (str)content: The content of the message
+    (str)email: The email to send the message to
+    
+    """
+    acc = CONFIG['email']
+    SMTP_SERVER, SMTP_PORT = CONFIG['smtpSenderServer'], 587
+    sender_email, sender_passowrd = acc['email'], acc['password']
+
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = email
+    message['Subject'] = 'Hi helo'
+
+    message.attach(MIMEText(content, 'plain'))
+
+    with smtplib.SMTP(
+        SMTP_SERVER, SMTP_PORT
+    ) as smtp:
+        smtp.ehlo()
+        smtp.starttls(context=ssl.create_default_context())
+        smtp.ehlo()
+        smtp.login(sender_email, sender_passowrd)
+        smtp.sendmail(sender_email, email, message.as_string())
+
+
+if __name__ == "__main__":
+    send_message("testing", "4076860266@tmomail.net")
