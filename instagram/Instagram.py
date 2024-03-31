d6172743694863356ab0f738a3c9ad7d53685321
Austin McCallister <austinmccallister0@gmail.com>
2024-03-31 19:06:43 -0400

    Instagram post support
    Can now make posts on instagram automatically

---
+++ instagram/Instagram.py
@@ -0,0 +1,23 @@
+from instagrapi import Client
+
+
+
+class Instagram():
+    def __init__(self, username: str, password: str) -> None:
+        self.cl = Client()
+        self.cl.login(username, password)
+
+    def uploadPost(self, imagePath: str, caption: str=None) -> None:
+        """ Creates a post with an image path and a caption
+        
+        Parameters:
+        (str)imagePath: A path to the image
+        (str)caption: A caption for the post; Defaults to None
+        
+        """
+
+        media = self.cl.photo_upload(
+            path=imagePath,
+            caption=caption
+
+        )
---
+++ instagram/__init__.py
@@ -0,0 +1,1 @@
+from .Instagram import *
