bd4efb513a8d93c4a0bc761844d44fc3a6178c62
Austin McCallister <austinmccallister0@gmail.com>
2024-03-31 23:42:46 -0400

    Converted to class structure
    Also removed the CONFIG variable; this is now managed in the main file

--- message/imaptosms/imaptosms.py
+++ message/imaptosms/imaptosms.py
@@ -1,41 +1,41 @@
 import ssl
 import smtplib
-import json
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-CONFIG = None
-with open("./config.json", "r") as f:
-    CONFIG = json.load(f)
 
-def send_message(content: str, email: str) -> None:
-    """ Sends a message with IMAP - Used to send messages over SMS
-    
-    Parameters:
-    (str)content: The content of the message
-    (str)email: The email to send the message to
-    
-    """
-    acc = CONFIG['email']
-    SMTP_SERVER, SMTP_PORT = acc['smtpServer'], 587
-    sender_email, sender_passowrd = acc['email'], acc['password']
+class ImapToSMS:
+    def __init__(self, smtpServer, email, pwd) -> None:
+        self.smtpServer = smtpServer
+        self.email = email
+        self.pwd = pwd
+        self.SMTP_PORT = 587
 
-    message = MIMEMultipart()
-    message['From'] = sender_email
-    message['To'] = email
-    message['Subject'] = 'Hi helo'
+    def send_message(self, content: str, email: str) -> None:
+        """ Sends a message with IMAP - Used to send messages over SMS
+        
+        Parameters:
+        (str)content: The content of the message
+        (str)email: The email to send the message to
+        
+        """
 
-    message.attach(MIMEText(content, 'plain'))
+        message = MIMEMultipart()
+        message['From'] = self.email
+        message['To'] = email
+        message['Subject'] = 'Hi helo'
 
-    with smtplib.SMTP(
-        SMTP_SERVER, SMTP_PORT
-    ) as smtp:
-        smtp.ehlo()
-        smtp.starttls(context=ssl.create_default_context())
-        smtp.ehlo()
-        smtp.login(sender_email, sender_passowrd)
-        smtp.sendmail(sender_email, email, message.as_string())
+        message.attach(MIMEText(content, 'plain'))
+
+        with smtplib.SMTP(
+            self.smtpServer, self.SMTP_PORT
+        ) as smtp:
+            smtp.ehlo()
+            smtp.starttls(context=ssl.create_default_context())
+            smtp.ehlo()
+            smtp.login(self.email, self.pwd)
+            smtp.sendmail(self.email, email, message.as_string())
 
 
-if __name__ == "__main__":
-    send_message("hi how is ur day", "4076860266@tmomail.net")
+    if __name__ == "__main__":
+        send_message("hi how is ur day", "4076860266@tmomail.net")
