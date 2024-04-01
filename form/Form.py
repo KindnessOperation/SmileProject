7fdb23fbcf5d85d456147e8cd4d69df4ddd7cb50
Austin McCallister <austinmccallister0@gmail.com>
2024-03-31 23:44:05 -0400

    Converted to class structure

--- form/Form.py
+++ form/Form.py
@@ -1,53 +1,57 @@
 from apiclient import discovery
 from httplib2 import Http
 from oauth2client import client, file, tools
-import json
 from typing import Generator
 
-CONFIG = None
-with open("../config.json") as f:
-    CONFIG = json.load(f)
 
-SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly" # Only scope needed
-DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+class Form:
+    def __init__(self, formId: str) -> None:
+        self.SCOPES = "https://www.googleapis.com/auth/forms.responses.readonly" # Only scope needed
+        self.DISCOVERY_DOC = "https://forms.googleapis.com/$discovery/rest?version=v1"
+        self.formId = formId
 
-def _authenticate() -> client.OAuth2Credentials:
-    """ Authenticates with the Google Cloud API with 2auth
-    
-    Returns:
-    (client.OAuth2Credentials): 2Oath Credential object
-    
-    """
-    store = file.Storage("./token.json")
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(r"../service_account.json", SCOPES)
-        creds = tools.run_flow(flow, store)
-    return creds
+    def _authenticate(self) -> client.OAuth2Credentials:
+        """ Authenticates with the Google Cloud API with 2auth
+        
+        Returns:
+        (client.OAuth2Credentials): 2Oath Credential object
+        
+        """
+        store = file.Storage("./token.json")
+        creds = store.get()
+        if not creds or creds.invalid:
+            flow = client.flow_from_clientsecrets(r"./service_account.json", self.SCOPES)
+            creds = tools.run_flow(flow, store)
+        return creds
 
-def getResponses() -> Generator[tuple[str, str], None, None]:
-    """ Gets all form results as strings 
-    
-    Returns:
-    (Generator[tuple[str, str], None, None]): Yields each response as a tuple with the responseId and its value
-    
-    """
+    def getResponses(self) -> Generator[tuple[str, str], None, None]:
+        """ Gets all form results as strings 
+        
+        Returns:
+        (Generator[tuple[str, str], None, None]): Yields each response as a tuple with the responseId and its value
+        
+        """
 
-    creds = _authenticate()
-    service = discovery.build(
-        "forms",
-        "v1",
-        http=creds.authorize(Http()),
-        discoveryServiceUrl=DISCOVERY_DOC,
-        static_discovery=False,
-    )
+        creds = self._authenticate()
+        service = discovery.build(
+            "forms",
+            "v1",
+            http=creds.authorize(Http()),
+            discoveryServiceUrl=self.DISCOVERY_DOC,
+            static_discovery=False,
+        )
 
-    result = service.forms().responses().list(formId=CONFIG['formId']).execute()
-    responses = result['responses']
-    for response in responses:
-        yield (response['responseId'], response['answers']['06b405aa']['textAnswers']['answers'][0]['value'])
+        result = service.forms().responses().list(formId=self.formId).execute()
+        responses = result['responses']
+        for response in responses:
+            yield (response['responseId'], response['answers']['06b405aa']['textAnswers']['answers'][0]['value'])
 
-
-
-if __name__ == "__main__":
-    print(list(getResponses()))
+    def getResponse_pks(self) -> list:
+        """ Returns a list of response ids used for initialization
+        
+        Returns:
+        (list): List of response ids
+        
+        """
+        responses = list(self.getResponses())
+        return [response[0] for response in responses] # Parse and pick out the responseId and put into a list
