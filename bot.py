446cd4a03df3f8b6c1c1d7a4330f86587016b577
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2024-04-11 05:53:16 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -148,7 +148,8 @@ async def on_reaction_add(reaction: discord.Reaction, user: discord.Member) -> N
 
 @bot.command()
 async def verify(ctx: commands.Context) -> None:
-    referenced_message = ctx.message.reference
+    referenced_message_id = ctx.message.reference.message_id
+    referenced_message = await ctx.fetch_message(referenced_message_id)
     if (not referenced_message):
         await ctx.send(embed=discord.Embed(title="No message referenced", color=discord.Color.red()))
         return
