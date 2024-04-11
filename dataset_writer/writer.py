152eedad1a9292a608fe0d1a6d1430584f2956cf
Austin McCallister <austinmccallister0@gmail.com>
2024-04-10 21:30:42 -0400

    Added school to data

--- dataset_writer/writer.py
+++ dataset_writer/writer.py
@@ -9,16 +9,17 @@ class CSVWriter():
     def __del__(self) -> None:
         self.file.close()
 
-    def writeData(self, timestamp: str, response: str, kind: bool) -> None:
+    def writeData(self, timestamp: str, response: str, kind: bool, school: str) -> None:
         """ Adds the response to the dataset
         
         Parameters:
         (str)timestamp: The time that the response was sent to the Google for within 5 seconds of accuracy
         (str)response: The response from the form
         (bool)kind: Determinate if the message was kind or not
+        (str)school: The name of the school
         
         """
-        self.writer.writerow([timestamp, response, int(kind)])
+        self.writer.writerow([timestamp, school, response, int(kind)])
         
     
 
