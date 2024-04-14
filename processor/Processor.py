76d8e9ef6a133fc618b0696cbda5f69e8f0a769d
Austin McCallister <austinmccallister0@gmail.com>
2024-04-14 15:30:27 -0400

    Supports emojis with variation selectors

--- processor/Processor.py
+++ processor/Processor.py
@@ -67,17 +67,16 @@ def createPostImage(response: str, backgroundURI: str) -> Image.Image:
 
     # Make draw object
     draw = ImageDraw.Draw(img)
-    modifiedResponse = "\n".join(["\n".join(textwrap.wrap(subtext, width=70)) for subtext in response.split("\n")]) # Response is separated by newlines so it doesn't run off the screen
+    response = "\n".join(["\n".join(textwrap.wrap(subtext, width=70)) for subtext in response.split("\n")]) # Response is separated by newlines so it doesn't run off the screen
     poppinsFont = ImageFont.truetype("./fonts/Poppins-Regular.ttf", 25)
     dancingScriptFont = ImageFont.truetype("./fonts/DancingScript.ttf", 55)
     emojiFont = ImageFont.truetype(r"fonts\AppleColorEmoji.ttf", 137)
     DANCINGHEIGHT = 50 # The height of the font is 50px since the text doesn't change
     PADDING = 80 # Padding between the two parts of text
 
-    _, poppinsHeight = getDimensions(draw, modifiedResponse, poppinsFont)
+    _, poppinsHeight = getDimensions(draw, response, poppinsFont)
 
     marginHeight = (1080 - (poppinsHeight + PADDING + DANCINGHEIGHT)) / 2 # [ Total height of image (1080) - PADDING (80) - content height of both pieces of text ] /2
-
 
     # Add the Dancing Script text first
 
@@ -86,17 +85,26 @@ def createPostImage(response: str, backgroundURI: str) -> Image.Image:
     logger.debug("Added dancing script font text")
 
     # Now add the response
+    modifiedResponse = "".join(filter(lambda x: not (u"\uFE00" <= x <= "\uFE0F"), response)) # Filter through variation selectors
     draw.text((75, marginHeight+PADDING), modifiedResponse, fill=(255, 255, 255), font=poppinsFont)
     logger.debug("Added the response")
 
     # Now add any emojis that weren't embedded correctly
-    modifiedResponseL = modifiedResponse.split("\n")
-    for i, line in enumerate(modifiedResponseL):
+    responseL = response.split("\n")
+    for i, line in enumerate(responseL):
         for j, char in enumerate(line):
             if (not char.isascii()):
+
+
+                # Get variation selector if possible
+                if (j+1 != len(line) and not (var_selector := line[j+1]).isascii() and u"\uFE00" <= var_selector <= "\uFE0F"): # Checks if next char is unicode and in var selector range
+                    char += var_selector
                 
+                # If the current char is a var selector, ignore
+                if (u"\uFE00" <= char <= "\uFE0F"): continue
+
                 # Get the height of the text ABOVE the emoji in modifiedResponse
-                aboveText = "\n".join(modifiedResponseL[:i])
+                aboveText = "\n".join(responseL[:i])
                 _, aboveTextHeight = getDimensions(draw, aboveText, poppinsFont)
 
                 # The height that we paste at is aboveTextHeight + (marginHeight+PADDING) + (Some error)
@@ -120,7 +128,7 @@ def createPostImage(response: str, backgroundURI: str) -> Image.Image:
 
 
 if __name__ == "__main__":
-    createPostImage("""Brady Horn looks so peaceful when he sleeps \U0001f4a4 he's my cute little guy\U0001f970""",
+    createPostImage("""Sebbyyy Matoess you are so sweet and thoughtful, I'm so glad that I got to meet you at school. You give me a good start to my day at school and always cheer me up when I'm down, ilyyy \u2764\ufe0f""",
                     "https://images.unsplash.com/photo-1615839377917-bc950e77a6d1?ixid=M3w1ODU1ODV8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4NTQ0NDd8&ixlib=rb-4.0.3?w=1080&h=1080&fit=crop").show()
     # emojiFont = ImageFont.truetype(r"fonts\AppleColorEmoji.ttf", 137)
     # getEmojiMask(emojiFont, "\U0001f602", (100, 100)).show()
