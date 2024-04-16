b466ac9ec177b8157ce1c3fb11402fd392f889b1
Austin McCallister <austinmccallister0@gmail.com>
2024-04-15 21:00:03 -0400

    Two notes in one wallpaper support

--- genWallpeper.py
+++ genWallpeper.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-
+import threading
 
 def get_dominant_color(pil_img):
     img = pil_img.copy()
@@ -9,17 +9,37 @@ def get_dominant_color(pil_img):
     dominant_color = img.getpixel((0, 0))
     return dominant_color
 
-
 IMAGESPATH = "./images/out"
+images = os.listdir(IMAGESPATH)
 
-for file in os.listdir(IMAGESPATH):
-    img = Image.open(os.path.join(IMAGESPATH, file))
-    color = (0, 0, 0, 255)
-    while (color != (0, 0, 0, 255)): # Sometimes it bugs and give (0, 0, 0, 255) by default
-        color = get_dominant_color(img)
-    background = Image.new(size=(1170, 2532), color=color, mode="RGBA")
+def task(i: int) -> None:
+    if (i+1 == len(images)): return # Even number of images; Ignore last
 
-    img = img.resize((1170, 1170))
-    background.paste(img, (0, 2532-1170-222))
+    path1 = os.path.join(IMAGESPATH, images[i])
+    path2 = os.path.join(IMAGESPATH, images[i+1])
 
-    background.save("./images/out/processed/%s" % file)
+    if (os.path.isdir(path1) or os.path.isdir(path2)): return
+
+    img = Image.open(path1)
+    img2 = Image.open(path2)
+
+    background = Image.new(size=(1170, 2532), mode="RGBA")
+
+    img = img.resize((1170, 1266))
+    img2 = img2.resize((1170, 1266))
+
+
+    background.paste(img, (0, 0))
+    background.paste(img2, (0, 2532-1266))
+
+    filename = "%s_%s" % (images[i].split(".")[0], images[i+1])
+    background.save("./images/out/processed/%s" % filename)
+
+threads: list[threading.Thread] = []
+for i in range(0, len(images), 2):
+    thread = threading.Thread(None, task, args=(i,))
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()    
